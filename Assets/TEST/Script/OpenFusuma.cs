c78b83a1b02b937f85dc57251cad1aa50df82e26
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-09-25 19:33:04 +0900

    襖を開けたい

---
+++ Assets/TEST/Script/OpenFusuma.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFusuma : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject roomCenter;
+    [SerializeField] float distance;
+    [SerializeField] float targetDistance;
+    [SerializeField] bool isOpen;
+    [SerializeField] bool isClose;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position,roomCenter.transform.position);
+        if(distance<targetDistance && isOpen)
+        {
+            anim.SetBool("isOpen", true);
+            isOpen = true;
+            isClose = false;
+        }
+        if (distance > targetDistance && isClose)
+        { 
+            anim.SetBool("isClose", true);
+            isClose = true;
+            isOpen = false;
+        }
+    }
+}
--- Assets/TEST/Script/PlayerCamera.cs
+++ Assets/TEST/Script/PlayerCamera.cs
@@ -16,8 +16,8 @@ public class PlayerCamera : MonoBehaviour
     void Start()
     {
     //マウスポインターを真ん中に固定し見えなくする
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
