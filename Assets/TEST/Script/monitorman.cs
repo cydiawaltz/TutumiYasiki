be8a4419f50def86369125da4e57866457360c37
TKKmiyajima <135396758+cydiawaltz@users.noreply.github.com>
2025-05-12 16:44:36 +0900

    URP前最後

--- Assets/TEST/Script/monitorman.cs
+++ Assets/TEST/Script/monitorman.cs
@@ -1,10 +1,10 @@
-using UnityEditor.Animations;
+
 using UnityEngine;
 
 public class monitorman : MonoBehaviour
 {
     [SerializeField] Camera MainCam;
-    [SerializeField] AnimatorController[] controller;//[0]idle [1]walk
+    [SerializeField] RuntimeAnimatorController[] controller;//[0]idle [1]walk
     [SerializeField] Animator animator;
     [SerializeField] State state;
     enum State
@@ -14,7 +14,7 @@ public class monitorman : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        MainCam.cullingMask = (1 << 1) + (1 << 2) + (1 << 3) + (1 << 4) + (1 << 5) + (1 << 6) + (1 << 7);
+        MainCam.cullingMask = (1 << 1) + (1 << 2) + (1 << 3) + (1 << 4) + (1 << 5) + (1 << 6) + (1 << 7) + (1 << 9);
     }
 
     // Update is called once per frame
